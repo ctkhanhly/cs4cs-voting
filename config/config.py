eb47f59ffbe4239bcf961726a43c67d55bb256cb
ctkhanhly <ctkhanhly@gmail.com>
2019-08-02 07:48:47 +0700

    chaned config.py to use json file locally and heroku database_url env for production

--- config/config.py
+++ config/config.py
@@ -5,13 +5,15 @@ import json
 # dotenv_path = join(dirname(__file__), '.env')
 # load_dotenv(dotenv_path)
 
-path_config_json = os.path.dirname(os.path.abspath(__file__)) + '/config.json'
+
 DATABASE_URL =""
-with open(path_config_json) as f:
-    config_json = json.load(f)
-    f.close()
 if os.getenv("FLASK_ENV") == "development":
+    path_config_json = os.path.dirname(os.path.abspath(__file__)) + '/config.json'
+    with open(path_config_json) as f:
+        config_json = json.load(f)
+        f.close()
     DATABASE_URL = config_json["development"]["DATABASE_URL"]
+    #DATABASE_URL = os.getenv("DATABASE_URL")
 else:
     #DATABASE_URL = config_json["production"]["DATABASE_URL"]
     DATABASE_URL = os.getenv("DATABASE_URL")
